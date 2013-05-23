789a58cf0192dc22b8e81cbd7a351622defc97b0
Mindaugas D <mindaugas.dragunas@gmail.com>
2013-05-23 18:46:05 +0300

    Projektas Git'e

---
+++ OptimizingAds/OptimizingAds/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace OptimizingAds
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ OptimizingAds/OptimizingAds/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OptimizingAds.MvcApplication" Language="C#" %>
