d68aefb3b63f9adc5aa473474270d9b8860050ea
Audrius <momantai0@gmail.com>
2013-06-06 13:58:13 +0300

    Sutvarkytas Login
    Ištryniau Gintauto Loginą layout'e, sutvarkiau pagal Dariaus daryt�
    loginą, kitų pakeitimų nedėjau.

---
+++ OptimizingAds/OptimizingAds/ApiClient/CampaignManagementService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ApiClient.AdvertiserService;
+using ApiClient.CampaignService;
+
+namespace ApiClient
+{
+    public class CampaignManagementService
+    {
+        public static IDictionary<int, string> GetAdvertisers(string ticket)
+        {
+            var client = new AdvertiserServiceClient();
+            return client.GetAdvertisers(ticket, new GetAdvertisersData())
+                         .Select(p => new {p.Id, p.Name})
+                         .ToDictionary(kvp => kvp.Id.Value, kvp => kvp.Name);
+        }
+
+        public static IDictionary<int, string> GetCampaigns(string ticket, int advertiserId)
+        {
+            var client = new CampaignServiceClient();
+            return client.GetCampaigns(ticket, new GetCampaignsData
+                                                   {
+                                                       AdvertiserId = advertiserId
+                                                   })
+                         .Select(p => new {p.Id, p.Name})
+                         .ToDictionary(kvp => kvp.Id.Value, kvp => kvp.Name);
+        }
+    }
+}
---
+++ OptimizingAds/OptimizingAds/ApiClient/LoginService.cs
@@ -0,0 +1,19 @@
+﻿using ApiClient.SecurityService;
+
+namespace ApiClient
+{
+    public class LoginService
+    {
+        public static string Login(string login, string password)
+        {
+            var client = new SecurityServiceClient();
+
+            return client.Login(new LoginData
+                             {
+                                 Password = password,
+                                 UserName = login
+                             });
+        }
+    }
+}
+
